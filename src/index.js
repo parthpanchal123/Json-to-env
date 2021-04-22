47e2775ef7ed3a8fd41416221bad0809c5a1fca1
Parth Panchal <parthpanchal53@gmail.com>
2021-04-22 21:22:39 +0530

    Done

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer
+  .prompt({
+    type: "input",
+    message: "Enter the name of the JSON file",
+    name: "fileName",
+  })
+  .then(({ fileName }) => {
+    if (!fileName) {
+      console.log("\x1b[31m", "Enter a valid JSON file");
+      return;
+    }
+    try {
+      const cwd = process.cwd();
+      const rawdata = fs.readFileSync(cwd + `/${fileName}`);
+      const jsonData = JSON.parse(rawdata);
+
+      let envFile = "";
+      for (const key of Object.keys(jsonData)) {
+        envFile += `${key}=${jsonData[key]}\n`;
+      }
+
+      fs.writeFileSync(".env", envFile);
+
+      console.log("\x1b[34m", ".env created successfully");
+      //file written successfully
+    } catch (err) {
+      console.log("\x1b[31m", err.message);
+    }
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+      console.log("\x1b[31m", "Prompt not supported here");
+    } else {
+      // Something else went wrong
+      console.log("\x1b[31m", "Something went wrong");
+    }
+  });
